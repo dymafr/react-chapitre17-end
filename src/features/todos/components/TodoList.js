b29f39b164ea4b952ec4ec7496e987058112bbd7
hades <quentindevial19@gmail.com>
2018-12-26 12:06:20 +0100

    first commit

---
+++ src/features/todos/components/TodoList.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+import { connect } from 'react-redux';
+import { toggleTodo, deleteTodo, fetchTodo } from '../store/actions';
+import { filteredTodoDataSelector } from '../store/selectors';
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    props.fetchTodo();
+  }
+
+  render() {
+    const { todos, deleteTodo, toggleTodo } = this.props;
+    return (
+      <ul className="list-group">
+        { todos && todos.map( (t, i) => (
+          <TodoItem 
+            key={ t.name } 
+            todo={ t } 
+            deleteTodo={ () => deleteTodo(i) } 
+            toggleTodo={ () => toggleTodo(i) }
+          />
+        )) }
+      </ul>
+    )
+  }
+}
+
+export default connect((state, ownProps) => {
+  const filter = ownProps.match.params.filter;
+  return {
+    todos: filteredTodoDataSelector(state, filter)
+  }
+}, {
+  toggleTodo,
+  deleteTodo,
+  fetchTodo
+})(TodoList);
---
+++ src/features/todos/store/actions.js
@@ -0,0 +1,92 @@
+import apiFirebase from '../../../config/api.firebase';
+
+export const DELETE_TODO = 'delete todo';
+export const TOGGLE_TODO = 'toggle todo';
+
+export const TRY_ADD_TODO = 'try add todo';
+export const ADD_TODO_SUCCESS = 'add todo success';
+export const ADD_TODO_ERROR = 'add todo error';
+
+
+export const REQUEST_TODO = 'request todo';
+export const FETCH_TODO = 'fetch todo';
+export const FETCH_TODO_SUCCESS = 'fetch todo success';
+export const FETCH_TODO_ERROR = 'fetch todo error';
+
+export const tryAddTodo = (todo) => {
+  return (dispatch, getState) => {
+    const todos = [ ...getState().todos.data, todo ];
+    return apiFirebase.put('todos.json', todos).then(
+      response => dispatch(addTodoSuccess(todo)),
+      error => dispatch(addTodoError(error))
+    )
+
+  }
+}
+
+export const addTodoSuccess = (todo) => {
+  return {
+    type: ADD_TODO_SUCCESS,
+    todo
+  }
+}
+
+export const addTodoError = (error) => {
+  return {
+    type: ADD_TODO_ERROR,
+    error
+  }
+} 
+
+
+export const deleteTodo = (index) => {
+  return {
+    type: DELETE_TODO,
+    index
+  }
+}
+
+
+export const toggleTodo = (index) => {
+  return {
+    type: TOGGLE_TODO,
+    index
+  }
+}
+
+export const requestTodo = () => {
+  return {
+    type: REQUEST_TODO
+  }
+}
+
+export const fetchTodoSuccess = (todos) => {
+  return {
+    type: FETCH_TODO_SUCCESS,
+    todos
+  }
+}
+
+export const fetchTodoError = (error) => {
+  return {
+    type: FETCH_TODO_ERROR,
+    error
+  }
+}
+
+export const fetchTodo = () => {
+  return (dispatch) => {
+    dispatch(requestTodo());
+    return apiFirebase.get('todos.json').then(
+      (response) => {
+        const data = response.data;
+        dispatch(fetchTodoSuccess(data));
+      },
+      (error) => {
+        dispatch(fetchTodoError(error));
+      }
+    )
+  }
+}
+
+
