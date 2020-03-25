d7008b1a7d4f0a05e5c545cc25071c8984d5a1bb
hades <quentindevial19@gmail.com>
2020-03-25 13:14:51 +0100

    update packages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import Loadable from 'react-loadable';
+import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Loadable from "react-loadable";
 
 const LazyTodos = Loadable({
-  loader: () => import(/* webpackChunkName: 'todos' */'./features/todos'),
+  loader: () => import(/* webpackChunkName: 'todos' */ "./features/todos"),
   loading: () => <h1> Loading ... </h1>
-})
+});
 
 class App extends Component {
   render() {
     return (
       <div className="container p-5">
-        <Route path="/todos" component={ LazyTodos } />
+        <Switch>
+          <Route path="/todos" component={LazyTodos} />
+          <Redirect to="/todos" />
+        </Switch>
       </div>
     );
   }
