e423ba1342e2252511e4351739c076e655afea88
hades <quentindevial19@gmail.com>
2020-03-28 14:29:44 +0100

    feat: update lazy loading

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, { Component } from "react";
+import React, { Component, lazy, Suspense } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-import Loadable from "react-loadable";
 
-const LazyTodos = Loadable({
-  loader: () => import(/* webpackChunkName: 'todos' */ "./features/todos"),
-  loading: () => <h1> Loading ... </h1>
-});
+const LazyTodos = lazy(() =>
+  import(/* webpackChunkName: "todos" */ "./features/todos")
+);
 
 class App extends Component {
   render() {
     return (
       <div className="container p-5">
-        <Switch>
-          <Route path="/todos" component={LazyTodos} />
-          <Redirect to="/todos" />
-        </Switch>
+        <Suspense fallback={<h1>Loading ...</h1>}>
+          <Switch>
+            <Route path="/todos" component={LazyTodos} />
+            <Redirect to="/todos" />
+          </Switch>
+        </Suspense>
       </div>
     );
   }
