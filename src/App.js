d98130a9c6ae6e6beb5c86bd5ee5e1695a1c4163
Erwan Vallée <erwanp.vallee@gmail.com>
2020-03-29 20:10:55 +0200

    fix: routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React, { Component, lazy, Suspense } from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route } from "react-router-dom";
 
-const LazyTodos = lazy(() =>
-  import(/* webpackChunkName: "todos" */ "./features/todos")
-);
+const LazyTodos = lazy(() => import("./features/todos"));
 
 class App extends Component {
   render() {
     return (
       <div className="container p-5">
         <Suspense fallback={<h1>Loading ...</h1>}>
-          <Switch>
-            <Route path="/todos" component={LazyTodos} />
-            <Redirect to="/todos" />
-          </Switch>
+          <Route path="/" component={LazyTodos} />
         </Suspense>
       </div>
     );
